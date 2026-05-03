81583bd1a436b77d044af5dba7a04c296d77b8bc
MrChildrenJ <jj.chunchieh.huang@gmail.com>
2026-05-03 11:43:12 -0600

    add memory & string operation funcions: memcpy(), memset(), strcpy(), strcmp()

--- common.c
+++ common.c
@@ -65,3 +65,44 @@ void printf(const char* fmt, ...) {
 end:
     va_end(vargs);
 }
+
+// == Memory Operation ==
+void* memcpy(void* dst, const void* src, size_t n) {
+    // Transform dst & src to d & s, becos void* can't be directly dereferenced or used in pointer arithmetic
+    uint8_t* d = (uint8_t*) dst;
+    const uint8_t *s = (const uint8_t *) src;
+    while (n--)
+        *d++ = *s++;
+    return dst;
+}
+
+void* memset(void* buf, char c, size_t n) {
+    uint8_t* p = (uint8_t*) buf;
+    while (n--)
+        *p++ = c;
+    return buf;
+}
+
+
+// == String Operation ==
+char *strcpy(char* dst, const char* src) {
+    char* d = dst;
+    while (*src)        // keep copying even if src is longer than dst, NEVER use it in production
+        *d++ = *src++;  // use strlcpy
+    *d = '\0';          // strncpy doesn't guarantee ending with \0, and uses \0 for padding remaining space
+    return dst;
+}
+
+int strcmp(const char* s1, const char* s2) {    // equal -> return 0 (counter-intuitive...)
+    while (*s1 && *s2 && *s1 == *s2) {
+        s1++;
+        s2++;
+    }
+    // while (*s1 && *s2) {
+    //     if (*s1 != *s2)
+    //         break;
+    //     s1++;
+    //     s2++;
+    // }
+    return *(unsigned char*)s1 - *(unsigned char*)s2;
+}
--- common.h
+++ common.h
@@ -1,9 +1,29 @@
 #pragma once
 
+typedef int bool;
+typedef unsigned char uint8_t;
+typedef unsigned short uint16_t;
+typedef unsigned int uint32_t;
+typedef unsigned long long uint64_t;
+typedef uint32_t size_t;
+typedef uint32_t paddr_t;               // physical memory addresses
+typedef uint32_t vaddr_t;               // virtual memory addresses = uintptr_t in stdlib
+
+#define true  1
+#define false 0
+#define NULL  ((void *) 0)
+#define align_up(value, align)   __builtin_align_up(value, align)   // align must be power of 2
+#define is_aligned(value, align) __builtin_is_aligned(value, align) // checks if value is a multiple of align
+#define offsetof(type, member)   __builtin_offsetof(type, member)   // calculates the offset in bytes of a specific member from the beginning of its parent structure or union
+
 // variable argument handling
 #define va_list  __builtin_va_list      // 一個型別，用來宣告「指向當前讀取位置」的游標變數
 #define va_start __builtin_va_start     // 初始化游標，讓它指向 fmt 之後第一個可變參數的位置
 #define va_end   __builtin_va_end       // Clean up when done
 #define va_arg   __builtin_va_arg       // 從游標位置讀取一個 type 型別的值，並將游標前進
 
+void *memset(void *buf, char c, size_t n);
+void *memcpy(void *dst, const void *src, size_t n);
+char *strcpy(char *dst, const char *src);
+int strcmp(const char *s1, const char *s2);
 void printf(const char *fmt, ...);  
